cd31f5542f0dad1bf2489e61313f6be861ca5361
harmless-tech <git@harmless.tech>
2025-02-07 23:33:09 -0500

    Reduce unsafe surface in unix extensions

--- src/writev.rs
+++ src/writev.rs
@@ -8,51 +8,46 @@ pub trait WritevExt {
 
 #[cfg(unix)]
 mod imp {
-    use libc;
     use std::os::unix::io::AsRawFd;
 
-    use super::*;
+    use super::{io, UdpSocket, WritevExt};
 
     impl WritevExt for UdpSocket {
         fn writev(&self, bufs: [&[u8]; 2]) -> io::Result<usize> {
-            unsafe {
-                let iovecs = [
-                    libc::iovec {
-                        iov_base: bufs[0].as_ptr() as *const _ as *mut _,
-                        iov_len: bufs[0].len(),
-                    },
-                    libc::iovec {
-                        iov_base: bufs[1].as_ptr() as *const _ as *mut _,
-                        iov_len: bufs[1].len(),
-                    },
-                ];
-                let r = libc::writev(self.as_raw_fd(), iovecs.as_ptr(), 2);
-                if r < 0 {
-                    Err(io::Error::last_os_error())
-                } else {
-                    Ok(r as usize)
-                }
+            let iovecs = [
+                libc::iovec {
+                    iov_base: bufs[0].as_ptr().cast_mut().cast(),
+                    iov_len: bufs[0].len(),
+                },
+                libc::iovec {
+                    iov_base: bufs[1].as_ptr().cast_mut().cast(),
+                    iov_len: bufs[1].len(),
+                },
+            ];
+            let r = unsafe { libc::writev(self.as_raw_fd(), iovecs.as_ptr(), 2) };
+            if r < 0 {
+                Err(io::Error::last_os_error())
+            } else {
+                Ok(r as usize)
             }
         }
 
         fn readv(&self, bufs: [&mut [u8]; 2]) -> io::Result<usize> {
-            unsafe {
-                let mut iovecs = [
-                    libc::iovec {
-                        iov_base: bufs[0].as_mut_ptr() as *mut _,
-                        iov_len: bufs[0].len(),
-                    },
-                    libc::iovec {
-                        iov_base: bufs[1].as_mut_ptr() as *mut _,
-                        iov_len: bufs[1].len(),
-                    },
-                ];
-                let r = libc::readv(self.as_raw_fd(), iovecs.as_mut_ptr(), 2);
-                if r < 0 {
-                    Err(io::Error::last_os_error())
-                } else {
-                    Ok(r as usize)
-                }
+            let mut iovecs = [
+                libc::iovec {
+                    iov_base: bufs[0].as_mut_ptr().cast(),
+                    iov_len: bufs[0].len(),
+                },
+                libc::iovec {
+                    iov_base: bufs[1].as_mut_ptr().cast(),
+                    iov_len: bufs[1].len(),
+                },
+            ];
+            let r = unsafe { libc::readv(self.as_raw_fd(), iovecs.as_mut_ptr(), 2) };
+            if r < 0 {
+                Err(io::Error::last_os_error())
+            } else {
+                Ok(r as usize)
             }
         }
     }
@@ -60,11 +55,11 @@ mod imp {
 
 #[cfg(windows)]
 mod imp {
-    use winapi::um::winsock2;
-    use winapi::shared::ws2def;
-    use winapi::shared::minwindef;
     use std::os::windows::io::AsRawSocket;
     use std::ptr;
+    use winapi::shared::minwindef;
+    use winapi::shared::ws2def;
+    use winapi::um::winsock2;
 
     use super::*;
 

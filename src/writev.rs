03b4499ec286b1d2403d9b2e57a6988ea366a576
harmless-tech <git@harmless.tech>
2025-02-08 15:32:03 -0500

    Use windows-sys instead of winapi

--- src/writev.rs
+++ src/writev.rs
@@ -1,5 +1,4 @@
-use std::io;
-use std::net::UdpSocket;
+use std::{io, net::UdpSocket};
 
 pub trait WritevExt {
     fn writev(&self, bufs: [&[u8]; 2]) -> io::Result<usize>;
@@ -12,6 +11,7 @@ mod imp {
 
     use super::{io, UdpSocket, WritevExt};
 
+    // TODO: Make iovecs pointer casts the same???
     impl WritevExt for UdpSocket {
         fn writev(&self, bufs: [&[u8]; 2]) -> io::Result<usize> {
             let iovecs = [
@@ -24,11 +24,12 @@ mod imp {
                     iov_len: bufs[1].len(),
                 },
             ];
-            let r = unsafe { libc::writev(self.as_raw_fd(), iovecs.as_ptr(), 2) };
+            // SAFETY: All params are setup in this function safely.
+            let r = unsafe { libc::writev(self.as_raw_fd(), iovecs.as_ptr(), iovecs.len() as _) };
             if r < 0 {
                 Err(io::Error::last_os_error())
             } else {
-                Ok(r as usize)
+                Ok(r.unsigned_abs())
             }
         }
 
@@ -43,11 +44,12 @@ mod imp {
                     iov_len: bufs[1].len(),
                 },
             ];
-            let r = unsafe { libc::readv(self.as_raw_fd(), iovecs.as_mut_ptr(), 2) };
+            // SAFETY: All params are setup in this function safely.
+            let r = unsafe { libc::readv(self.as_raw_fd(), iovecs.as_mut_ptr(), iovecs.len() as _) };
             if r < 0 {
                 Err(io::Error::last_os_error())
             } else {
-                Ok(r as usize)
+                Ok(r.unsigned_abs())
             }
         }
     }
@@ -55,73 +57,75 @@ mod imp {
 
 #[cfg(windows)]
 mod imp {
-    use std::os::windows::io::AsRawSocket;
-    use std::ptr;
-    use winapi::shared::minwindef;
-    use winapi::shared::ws2def;
-    use winapi::um::winsock2;
-
     use super::*;
+    use std::{os::windows::io::AsRawSocket, ptr};
+    use windows_sys::Win32::Networking::WinSock::{WSARecv, WSASend, WSABUF};
 
     impl WritevExt for UdpSocket {
         fn writev(&self, bufs: [&[u8]; 2]) -> io::Result<usize> {
-            unsafe {
-                let mut wsabufs = [
-                    ws2def::WSABUF {
-                        len: bufs[0].len() as winsock2::u_long,
-                        buf: bufs[0].as_ptr() as *const _ as *mut _,
-                    },
-                    ws2def::WSABUF {
-                        len: bufs[1].len() as winsock2::u_long,
-                        buf: bufs[1].as_ptr() as *const _ as *mut _,
-                    },
-                ];
-                let mut sent = 0;
-                let r = winsock2::WSASend(
-                    self.as_raw_socket() as usize,
+            // TODO: Check to make sure length is within a u32!
+
+            let mut wsabufs = [
+                WSABUF {
+                    len: bufs[0].len() as _, // TODO: Casts to u32!!!
+                    buf: bufs[0].as_ptr().cast_mut(),
+                },
+                WSABUF {
+                    len: bufs[1].len() as _, // TODO: Casts to u32!!!
+                    buf: bufs[1].as_ptr().cast_mut(),
+                },
+            ];
+            let mut sent = 0_u32;
+            // SAFETY: All params are setup in this function safely.
+            let r = unsafe {
+                WSASend(
+                    self.as_raw_socket() as _,
                     wsabufs.as_mut_ptr(),
-                    bufs.len() as minwindef::DWORD,
+                    bufs.len() as _,
                     &mut sent,
                     0,
                     ptr::null_mut(),
                     None,
-                );
-                if r == 0 {
-                    Ok(sent as usize)
-                } else {
-                    Err(io::Error::last_os_error())
-                }
+                )
+            };
+            if r == 0 {
+                Ok(sent as usize)
+            } else {
+                Err(io::Error::last_os_error())
             }
         }
 
         fn readv(&self, bufs: [&mut [u8]; 2]) -> io::Result<usize> {
-            unsafe {
-                let mut wsabufs = [
-                    ws2def::WSABUF {
-                        len: bufs[0].len() as winsock2::u_long,
-                        buf: bufs[0].as_mut_ptr() as *mut _,
-                    },
-                    ws2def::WSABUF {
-                        len: bufs[1].len() as winsock2::u_long,
-                        buf: bufs[1].as_mut_ptr() as *mut _,
-                    },
-                ];
-                let mut recved = 0;
-                let mut flags = 0;
-                let r = winsock2::WSARecv(
-                    self.as_raw_socket() as usize,
+            // TODO: Check to make sure length is within a u32!
+
+            let mut wsabufs = [
+                WSABUF {
+                    len: bufs[0].len() as _, // TODO: Casts to u32!!!
+                    buf: bufs[0].as_mut_ptr(),
+                },
+                WSABUF {
+                    len: bufs[1].len() as _, // TODO: Casts to u32!!!
+                    buf: bufs[1].as_mut_ptr(),
+                },
+            ];
+            let mut recved: u32 = 0;
+            let mut flags: u32 = 0;
+            // SAFETY: All params are setup in this function safely.
+            let r = unsafe {
+                WSARecv(
+                    self.as_raw_socket() as _,
                     wsabufs.as_mut_ptr(),
-                    bufs.len() as minwindef::DWORD,
+                    bufs.len() as _,
                     &mut recved,
                     &mut flags,
                     ptr::null_mut(),
                     None,
-                );
-                if r == 0 {
-                    Ok(recved as usize)
-                } else {
-                    Err(io::Error::last_os_error())
-                }
+                )
+            };
+            if r == 0 {
+                Ok(recved as usize)
+            } else {
+                Err(io::Error::last_os_error())
             }
         }
     }
